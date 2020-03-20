476695cbbdf59c4c423b96b4140b63dad61ce115
Yang <39217646+Fool-Yang@users.noreply.github.com>
2020-03-19 18:32:21 -0700

    Update test_train.py

--- test_train.py
+++ test_train.py
@@ -2,7 +2,7 @@ from utils.alpha_snake_zero_trainer import AlphaSnakeZeroTrainer
 from utils.alphaNNet import AlphaNNet
 start = int(input("Enter starting iteration:\n"))
 if start == 0:
-    net = AlphaNNet(input_shape = (21, 21, 3))
+    net = AlphaNNet(ins = (21, 21, 3))
 else:
     net = AlphaNNet(model = "models/nn" + str(start) + ".h5")
 a = AlphaSnakeZeroTrainer()
