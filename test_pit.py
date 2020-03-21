b8f3f5bd9355cdd89b61fe8620293ee2442eae31
Yang <39217646+Fool-Yang@users.noreply.github.com>
2020-03-21 15:39:25 -0700

    Update test_pit.py

--- test_pit.py
+++ test_pit.py
@@ -10,24 +10,54 @@ competeEps = 128
 
 file_name1 = input("\nEnter the model 1 name:\n")
 file_name2 = input("\nEnter the model 2 name:\n")
-t0 = time()
 nnet1 = AlphaNNet(model = "models/" + file_name1 + ".h5")
 nnet2 = AlphaNNet(model = "models/" + file_name2 + ".h5")
+sep = 1
+Alice = Agent(nnet1, range(sep))
+Bob = Agent(nnet2, range(sep, snake_cnt))
+win = 0
+draw = 0
+t0 = time()
+for _ in range(competeEps):
+    g = Game(height, width, snake_cnt)
+    winner_id = g.run(Alice, Bob, sep=sep)
+    if winner_id is None:
+        draw += 1
+    elif winner_id < sep:
+        win += 1
+print("1v3 WR =", file_name1, win/(competeEps), "DR = ", draw/(competeEps))
+print("Competing time", time() - t0)
+sep = 1
+Alice = Agent(nnet2, range(sep))
+Bob = Agent(nnet1, range(sep, snake_cnt))
+win = 0
+draw = 0
+t0 = time()
+for _ in range(competeEps):
+    g = Game(height, width, snake_cnt)
+    winner_id = g.run(Alice, Bob, sep=sep)
+    if winner_id is None:
+        draw += 1
+    elif winner_id < sep:
+        win += 1
+print("\n1v3 WR =", file_name2, win/(competeEps), "DR = ", draw/(competeEps))
+print("Competing time", time() - t0)
+snake_cnt = 2
 sep = snake_cnt//2
 Alice = Agent(nnet1, range(sep))
 Bob = Agent(nnet2, range(sep, snake_cnt))
 win = 0
 loss = 0
+t0 = time()
 for _ in range(competeEps):
     g = Game(height, width, snake_cnt)
     winner_id = g.run(Alice, Bob, sep=sep)
     if winner_id is None:
-        win += 1
-        loss += 1
+        pass
     elif winner_id < sep:
         win += 1
     else:
         loss += 1
-print("WR of", file_name1, win/(win + loss))
-print("WR of", file_name2, loss/(win + loss))
+print("\n1v1 WR =", file_name1, win/(competeEps))
+print("1v1 WR =", file_name2, loss/(competeEps))
 print("Competing time", time() - t0)
