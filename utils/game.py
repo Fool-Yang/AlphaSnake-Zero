8b4e3de73a45e32070c9af80c1c084942ccb8cb1
Yang <39217646+Fool-Yang@users.noreply.github.com>
2020-04-02 18:04:30 -0700

    merge game and snake

--- utils/game.py
+++ utils/game.py
@@ -1,8 +1,6 @@
 from random import sample, choice, random
 from numpy import array, reshape, rot90
 
-from utils.snake import Snake
-
 WALL = 1.0
 MY_HEAD = -1.0
 # mutipliers
@@ -312,3 +310,40 @@ class Game:
             f.write(str(row) + '\n')
         f.write('\n')
         f.close()
+
+
+class Snake:
+    
+    def __init__(self, ID, health, body):
+        self.id = ID
+        self.health = health
+        self.body = body
+    
+    def move(self, direction):
+        body = self.body
+        if direction == 0:   # up
+            y = body[0][0] - 1
+            x = body[0][1]
+        elif direction == 1: # right
+            y = body[0][0]
+            x = body[0][1] + 1
+        elif direction == 2: # down
+            y = body[0][0] + 1
+            x = body[0][1]
+        # if direction == 3: # left
+        else:
+            y = body[0][0]
+            x = body[0][1] - 1
+        body.insert(0, (y, x))
+        tail = body.pop()
+
+        # return the new head, the old head and the removed tail
+        # tells the Game how to up date the board sets
+        # don't remove the tail if it is on top of another body
+        if tail == self.body[-1]:
+            tail = None
+
+        return (body[0], body[1], tail)
+    
+    def grow(self):
+        self.body.append(self.body[-1])
