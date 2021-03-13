ccacb1113b43f1c6ecf5861590b83dd051bb7c09
Yang <39217646+Fool-Yang@users.noreply.github.com>
2021-03-12 21:40:38 -0500

    Update alpha_nnet.py

--- code/utils/alpha_nnet.py
+++ code/utils/alpha_nnet.py
@@ -16,35 +16,35 @@ class AlphaNNet:
             
             X = Input(input_shape)
             
-            H = Conv2D(256, (3, 3), use_bias = False, kernel_regularizer = l2(c))(X)
+            H = Conv2D(256, (3, 3), padding = "same", use_bias = False, kernel_regularizer = l2(c))(X)
             H = Activation('relu')(BatchNormalization(axis = 3)(H))
             
             # a residual block
-            H_shortcut = Cropping2D(cropping = 2)(H)
-            H = Conv2D(256, (3, 3), use_bias = False, kernel_regularizer = l2(c))(H)
-            H = Activation('relu')(BatchNormalization(axis = 3)(H))
-            H = Conv2D(256, (3, 3), use_bias = False, kernel_regularizer = l2(c))(H)
-            H = Activation('relu')(Add()([BatchNormalization(axis = 3)(H), H_shortcut]))
-            
-            H_shortcut = Cropping2D(cropping = 2)(H)
-            H = Conv2D(256, (3, 3), use_bias = False, kernel_regularizer = l2(c))(H)
-            H = Activation('relu')(BatchNormalization(axis = 3)(H))
-            H = Conv2D(256, (3, 3), use_bias = False, kernel_regularizer = l2(c))(H)
-            H = Activation('relu')(Add()([BatchNormalization(axis = 3)(H), H_shortcut]))
-            
-            H_shortcut = Cropping2D(cropping = 1)(H)
+            H_shortcut = H
             H = Conv2D(256, (3, 3), padding = "same", use_bias = False, kernel_regularizer = l2(c))(H)
             H = Activation('relu')(BatchNormalization(axis = 3)(H))
-            H = Conv2D(256, (3, 3), use_bias = False, kernel_regularizer = l2(c))(H)
+            H = Conv2D(256, (3, 3), padding = "same", use_bias = False, kernel_regularizer = l2(c))(H)
             H = Activation('relu')(Add()([BatchNormalization(axis = 3)(H), H_shortcut]))
             
-            H_shortcut = Cropping2D(cropping = 1)(H)
+            H_shortcut = H
             H = Conv2D(256, (3, 3), padding = "same", use_bias = False, kernel_regularizer = l2(c))(H)
             H = Activation('relu')(BatchNormalization(axis = 3)(H))
-            H = Conv2D(256, (3, 3), use_bias = False, kernel_regularizer = l2(c))(H)
+            H = Conv2D(256, (3, 3), padding = "same", use_bias = False, kernel_regularizer = l2(c))(H)
             H = Activation('relu')(Add()([BatchNormalization(axis = 3)(H), H_shortcut]))
             
-            H = Conv2D(2, (1, 1), use_bias = False, kernel_regularizer = l2(c))(H)
+            H_shortcut = H
+            H = Conv2D(256, (3, 3), padding = "same", use_bias = False, kernel_regularizer = l2(c))(H)
+            H = Activation('relu')(BatchNormalization(axis = 3)(H))
+            H = Conv2D(256, (3, 3), padding = "same", use_bias = False, kernel_regularizer = l2(c))(H)
+            H = Activation('relu')(Add()([BatchNormalization(axis = 3)(H), H_shortcut]))
+            
+            H_shortcut = H
+            H = Conv2D(256, (3, 3), padding = "same", use_bias = False, kernel_regularizer = l2(c))(H)
+            H = Activation('relu')(BatchNormalization(axis = 3)(H))
+            H = Conv2D(256, (3, 3), padding = "same", use_bias = False, kernel_regularizer = l2(c))(H)
+            H = Activation('relu')(Add()([BatchNormalization(axis = 3)(H), H_shortcut]))
+            
+            H = Conv2D(2, (1, 1), padding = "same", use_bias = False, kernel_regularizer = l2(c))(H)
             H = Activation('relu')(BatchNormalization(axis = 3)(H))
             
             Y = Activation('tanh')(Dense(3, kernel_regularizer = l2(c))(Flatten()(H)))
@@ -60,7 +60,7 @@ class AlphaNNet:
     def copy_and_compile(self, TPU = None):
         boundaries = [20, 40, 60, 80, 100]
         values = [0.0]*(len(boundaries) + 1)
-        n = 1e-5
+        n = 5e-6
         for i in range(len(boundaries)):
             values[i] = n
             n *= 0.25
