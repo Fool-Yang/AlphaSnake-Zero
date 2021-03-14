e1876845ead5b93fab1a92f2cb97ffda6c7c7055
Yang <39217646+Fool-Yang@users.noreply.github.com>
2021-03-14 12:10:37 -0400

    player

--- code/test_model.py
+++ code/test_model.py
@@ -1,8 +1,7 @@
 from utils.agent import Agent
 from utils.alpha_nnet import AlphaNNet
 from utils.mp_game_runner import MPGameRunner
-
-from utils.player import Player
+from player import Player
 
 height = 11
 width = 11
