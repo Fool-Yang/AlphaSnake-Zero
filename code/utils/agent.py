9425e9fe0ec2141b3cf31efb7f779e303bf8d19a
Yang <39217646+Fool-Yang@users.noreply.github.com>
2021-03-27 15:42:44 -0400

    softmax_base

--- code/utils/agent.py
+++ code/utils/agent.py
@@ -6,7 +6,7 @@ from utils.mp_game_runner import MCTSMPGameRunner
 
 class Agent:
     
-    def __init__(self, nnet, softmax_base = 100, training = False,
+    def __init__(self, nnet, softmax_base = 10, training = False,
                  max_MCTS_depth = 4, max_MCTS_breadth = 128):
         self.nnet = nnet
         self.softmax_base = softmax_base
