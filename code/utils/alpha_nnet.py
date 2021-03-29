295676b8c08a8ea605a9e312a3be2d5eaed59ad9
Yang <39217646+Fool-Yang@users.noreply.github.com>
2021-03-29 01:13:03 -0400

    Update alpha_nnet.py

--- code/utils/alpha_nnet.py
+++ code/utils/alpha_nnet.py
@@ -66,11 +66,11 @@ class AlphaNNet:
         center_x = len(X[0][0])//2
         for i in range(len(X)):
             # assign -1.0 to known obstacles
-            if X[i][center_y][center_x - 1][1] <= -0.04:
+            if X[i][center_y][center_x - 1][1] >= 0.04:
                 V[i][0] = -1.0
-            if X[i][center_y - 1][center_x][1] <= -0.04:
+            if X[i][center_y - 1][center_x][1] >= 0.04:
                 V[i][1] = -1.0
-            if X[i][center_y][center_x + 1][1] <= -0.04:
+            if X[i][center_y][center_x + 1][1] >= 0.04:
                 V[i][2] = -1.0
         return V
     
